fe24835bd9648473458549df3de2422c1495665a
Sir.WarFox <hweitani@gmail.com>
2017-11-27 20:47:24 +0700

    Premium Member Fragment done

--- app/src/main/java/com/example/sirwarfox/easydoctor/PremiumMemberFragmednt.java
+++ app/src/main/java/com/example/sirwarfox/easydoctor/PremiumMemberFragmednt.java
@@ -1,17 +1,22 @@
 package com.example.sirwarfox.easydoctor;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by Sir.WarFox on 27/11/2017.
  */
 
 public class PremiumMemberFragmednt extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,6 +26,15 @@ public class PremiumMemberFragmednt extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.premiumMembershipButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "CONGRATS FOR VIP MEMBER", Toast.LENGTH_LONG).show();
+                MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.horraysound);
+                mp.start();
+            }
+        });
 
     }
 }
